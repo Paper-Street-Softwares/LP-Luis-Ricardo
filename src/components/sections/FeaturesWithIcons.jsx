fe9fb1dfd1e02d378eab491ca3f1276e30e7de9e
Gabriel de Souza da Silva <gabrielsouza65115@gmail.com>
2025-08-20 08:22:49 -0300

    fix: Features

--- src/components/sections/FeaturesWithIcons.jsx
+++ src/components/sections/FeaturesWithIcons.jsx
@@ -60,7 +60,7 @@ export default function FeaturesWithIcons({ colorMode }) {
 
           <MotionDivDownToUp className="hidden desktop1:flex justify-center w-[35%]">
             <div
-              className="hidden h-[640px] w-full desktop1:flex col2 rounded-2xl bg-top bg-cover shadow-custom-opacity shadow-shadowFeatures/10"
+              className="hidden h-[640px] w-full desktop1:flex col2 rounded-2xl bg-bottom bg-cover shadow-custom-opacity shadow-shadowFeatures/10"
               style={{
                 backgroundImage: `url(${content.texts.features.imgFeatures})`,
               }}
